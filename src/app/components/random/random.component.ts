8f8a4199a6c4c0ae932adee997dfbc827de412b0
samdial69 <sdiallo585gmail.com>
2023-03-07 12:03:37 +0100

    Random done

--- src/app/components/random/random.component.ts
+++ src/app/components/random/random.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MusicModel} from "../../model/music.model";
+import {ApiService} from "../../api/api.service";
 
 @Component({
   selector: 'app-random',
   templateUrl: './random.component.html',
   styleUrls: ['./random.component.css']
 })
-export class RandomComponent {
+export class RandomComponent implements OnInit{
+  public music?: MusicModel | null;
+
+  constructor(
+    private apiService: ApiService,
+  ) {
+
+  }
+  ngOnInit(): void {
+    this.getRandomMusic();
+  }
+
+  getRandomMusic() {
+    this.apiService.getMusicRandom().subscribe((data) => {
+      this.music = data.body;
+    });
+  }
 
 }
